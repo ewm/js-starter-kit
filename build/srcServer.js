1c17ceb6a32ff19d1fea9b27c3ed207b48861c42
Eric Musso <ewmusso@gmail.com>
2018-03-25 22:39:36 -0400

    added configuration files, mock data api, need to add unit testing

---
+++ build/srcServer.js
@@ -0,0 +1,39 @@
+'use strict';
+import express from 'express';
+import path from 'path';
+import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
+
+
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function(req, res){
+  res.json([
+    {"id": 1, "firstName": "Bob", "lastName":"Smith"},
+    {"id": 2, "firstName": "Eric", "lastName":"Musso"},
+    {"id": 3, "firstName": "Dylan", "lastName":"Musso"}
+
+  ]);
+});
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
